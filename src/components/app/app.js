ba425ec44e823e9379e29fa7c11646f45c1ab864
fast <fast@fasts-MacBook-Pro.local>
2023-12-12 19:26:04 +0100

    [employers-list-item] Add states and props

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -5,19 +5,28 @@ import EmployersList from "../employers-list/employers-list";
 import EmployersAddForm from "../employers-add-form/employers-add-form";
 
 import './app.css'
+import {Component} from "react";
 
-function App() {
-    return (
-        <div className='app'>
-            <AppInfo/>
-            <div className="search-panel">
-                <SearchPanel/>
-                <AppFilter/>
+const data = [
+    {id: 1, name: "Gonzales", salary: "1000"},
+    {id: 2, name: "Pedro", salary: "2000"},
+    {id: 3, name: "Rodrigo", salary: "1500"}
+];
+
+class App extends Component {
+    render() {
+        return (
+            <div className='app'>
+                <AppInfo/>
+                <div className="search-panel">
+                    <SearchPanel/>
+                    <AppFilter/>
+                </div>
+                <EmployersList data={data}/>
+                <EmployersAddForm/>
             </div>
-            <EmployersList/>
-            <EmployersAddForm/>
-        </div>
-    );
+        );
+    }
 }
 
 export default App;
--- src/components/employers-list-item/employers-list-item.js
+++ src/components/employers-list-item/employers-list-item.js
@@ -1,23 +1,62 @@
-import './employers-list-item.css';
+import "./employers-list-item.css";
+import {Component} from "react";
 
-const EmployersListItem = ()=>{
-    return(
-        <li className="list-group-item d-flex justify-content-between">
-            <span className="list-group-item-label">John Smith</span>
-            <input type="text" className="list-group-item-input" defaultValue="1к$"/>
-            <div className='d-flex justify-content-center align-items-center'>
-                <button type="button"
-                    className="btn-cookie btn-sm ">
-                    <i className="fas fa-cookie"></i>
-                </button>
+class EmployersListItem extends Component{
 
-                <button type="button"
-                    className="btn-trash btn-sm ">
-                    <i className="fas fa-trash"></i>
-                </button>
-                <i className="fas fa-star"></i>
-            </div>
-        </li>
-    )
+    constructor(props){
+        super(props);
+
+        this.state = {
+            increase: false,
+            readyIncrease: false
+        };
+    }
+
+    onIncrease = () => {
+        this.setState(({increase})=>({
+            increase: !increase
+        }));
+    }
+
+    onReadyIncrease = () =>{
+        this.setState(({readyIncrease})=>({
+            readyIncrease: !readyIncrease
+        }));
+    }
+
+    render(){
+        const {name, salary} = this.props;
+        const {increase, readyIncrease} = this.state;
+        let classNames = "list-group-item d-flex justify-content-between";
+        if(increase){
+            classNames += " increase";
+        }
+        if(readyIncrease){
+            classNames += " like";
+        }
+
+        return(
+            <li className={classNames}>
+                <span className="list-group-item-label"
+                    onClick={this.onReadyIncrease}>
+                        {name}
+                    </span>
+                <input type="text" className="list-group-item-input" defaultValue={salary + "$"}/>
+                <div className='d-flex justify-content-center align-items-center'>
+                    <button type="button"
+                        className="btn-cookie btn-sm "
+                        onClick={this.onIncrease}>
+                        <i className="fas fa-cookie"></i>
+                    </button>
+
+                    <button type="button"
+                        className="btn-trash btn-sm ">
+                        <i className="fas fa-trash"></i>
+                    </button>
+                    <i className="fas fa-star"></i>
+                </div>
+            </li>
+        )
+    }
 };
 export default EmployersListItem;
