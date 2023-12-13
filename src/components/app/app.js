38fea5aa434253003e8faa42a69da1e6304b086e
fast <fast@fasts-MacBook-Pro.local>
2023-12-13 20:51:12 +0100

    [App, SearchPanel] Add search items

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -17,27 +17,28 @@ class App extends Component {
                 {id: 1, name: "Gonzales", salary: "1000", increase: false, raise: false},
                 {id: 2, name: "Pedro", salary: "2000", increase: false, raise: false},
                 {id: 3, name: "Rodrigo", salary: "1500", increase: false, raise: false}
-            ]
+            ],
+            term: ''
         }
     }
 
-    deleteItem = (id) =>{
-       this.setState(({data})=>{
-           return {data: data.filter(item=>item.id!==id)}
-       });
+    deleteItem = (id) => {
+        this.setState(({data}) => {
+            return {data: data.filter(item => item.id !== id)}
+        });
     };
 
-    createItem = (item) =>{
-        this.setState(({data})=>{
-            const id = Math.max(...data.map((item=>(item.id)))) + 1;
+    createItem = (item) => {
+        this.setState(({data}) => {
+            const id = Math.max(...data.map((item => (item.id)))) + 1;
             return {data: [...data, {id, ...item}]}
         });
     };
 
-    toggleProp = (id, prop) =>{
-        this.setState(({data})=>({
-            data: data.map((item)=>{
-                if(item.id===id){
+    toggleProp = (id, prop) => {
+        this.setState(({data}) => ({
+            data: data.map((item) => {
+                if (item.id === id) {
                     return {...item, [prop]: !item[prop]}
                 }
                 return item;
@@ -45,22 +46,35 @@ class App extends Component {
         }));
     };
 
+    onUpdateSearch = (term) => {
+        this.setState({term})
+    };
+
+    searchEmp = (data, term) => {
+        if (term.length === 0) {
+            return data;
+        }
+        return data.filter((item) => {
+            return item.name.indexOf(term) > -1
+        })
+    };
+
     render() {
-        const raised = this.state.data
-            .filter((item)=>item.raise)
-            .length;
+        const {data, term} = this.state;
+        const raised = data.filter((item) => item.raise).length;
+        const visibleData = this.searchEmp(data, term);
 
         return (
             <div className='app'>
                 <AppInfo
-                    employees={this.state.data.length}
+                    employees={data.length}
                     raised={raised}/>
                 <div className="search-panel">
-                    <SearchPanel/>
+                    <SearchPanel onUpdateSearch={this.onUpdateSearch}/>
                     <AppFilter/>
                 </div>
                 <EmployersList
-                    data={this.state.data}
+                    data={visibleData}
                     onDelete={this.deleteItem}
                     onToggleProp={this.toggleProp}/>
                 <EmployersAddForm
